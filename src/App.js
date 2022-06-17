212e23c5cbfcd0a1686dc79081bde84bd07cafe6
Zain Abidin <xan.abdn@gmail.com>
2022-06-17 11:05:19 +0100

    basic design for portifolio page added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import MenuBar from './components/MenuBar'
 import ContactForm from './components/Contact'
 import Banner from './components/Banner';
 import Footer from './components/footer';
+import Home from './components/Home'
+import Portfolio from './components/portfolio';
+
 
 function App() {
   return (
     <>
+    <Portfolio/>
     <Banner/>
+    <Home/>
     <ContactForm/>
     <Footer/>
+    
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className="px-3 mb-30">
+    <h1>Cover your page.</h1>
+    <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p>lOfficia amet ad anim nulla aliqua dolore deserunt. Consequat dolor occaecat culpa dolore consequat magna mollit. Lorem aliqua est quis aliquip aliqua. Ex labore irure dolor consectetur labore incididunt. Proident aliquip cupidatat occaecat id pariatur adipisicing aliqua minim sint irure. Esse veniam excepteur reprehenderit non sint labore id. In est reprehenderit dolore culpa quis pariatur cillum.</p>
+    <p className="lead">
+      <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+    </p>
+  </main>
+  )
+}
+
+export default Home
